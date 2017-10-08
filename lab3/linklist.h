87ec15da1525fa4b7458fbaf86b09207193dcbf2
892929826 <892929826@qq.com>
2017-10-08 16:49:32 +0800

    menu2

---
+++ lab3/linklist.h
@@ -0,0 +1,12 @@
+typedef struct DataNode
+{
+    char* cmd;
+    char* desc;
+    void (*handler)();
+    struct DataNode *next;
+} tDataNode;
+
+tDataNode* FindCmd(tDataNode *head, char *cmd);
+
+int ShowAllCmd(tDataNode *head);
+
